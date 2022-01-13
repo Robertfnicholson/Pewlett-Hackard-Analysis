b7e97ac657d16c754248c9dc5ecbca71b15f60f7
Bob Nicholson <93954896+Robertfnicholson@users.noreply.github.com>
2022-01-13 17:37:18 -0500

    Delete Pewlett-Hackard-Analysis_Folder directory

--- Pewlett-Hackard-Analysis_Folder/Queries/Employee_Database_challenge.sql
+++
@@ -1,6 +0,0 @@
--- -- From query results, create a table "Retirement_titles"
-SELECT e.first_name, e.last_name, e.emp_no, t.title, t.from_date, t.to_date
--- Into retirement_titles
-FROM titles As t
-Left Join employees as e, 
-On e.emp_no = t.emp_no;
